2b818548c6459de71210f49ed8f49ab51b3912fb
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-08 17:14:35 -0800

    Initial commit, gemfile lock, spec helper

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source('https://rubygems.org')
+
+gem('sinatra')
+gem('sinatra-contrib')
+gem('capybara')
+gem('rspec')
+gem('pg')
+gem('pry')
---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('client')
+require('stylist')
+require('pg')
+require('pry')
+
+DB = PG.connect({:dbname => hair_salon_test})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM clients *;")
+    DB.exec("Delete from stylists *;")
+  end
+end
