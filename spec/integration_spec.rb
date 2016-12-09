58378fe6a183198e6c397c7a91a06ab11f09672b
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-09 09:27:51 -0800

    Refactor integration specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,19 +4,10 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('adding a new stylist', {:type => :feature}) do
-  it('allows the user to add a stylist') do
+  it('allows the user to add a stylist and view their page') do
     visit('/')
     fill_in('stylist_name', :with => 'Milhouse Van Houten')
     click_button('Add a Stylist')
-    expect(page).to have_content('Milhouse Van Houten')
-  end
-end
-
-describe('viewing a stylist\'s page', {:type => :feature}) do
-  it('allows the user to see the profile page of a stylist with their clients') do
-    stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
-    stylist.save()
-    visit('/')
     click_link('Milhouse Van Houten')
     expect(page).to have_content('Milhouse Van Houten')
   end
