be3213b642ef823630c70ab8155240af4f51310e
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-09 09:45:57 -0800

    Refactor integration specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -21,8 +21,7 @@ describe('modifying a stylist\'s name', {:type => :feature}) do
   it('allows the user to change the name of stylist') do
     stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
     stylist.save()
-    visit('/')
-    click_link('Milhouse Van Houten')
+    visit("/stylists/#{stylist.id()}")
     fill_in('new_name', :with => 'Lenny')
     click_button('Change Name')
     expect(page).to have_content('Lenny')
@@ -33,8 +32,7 @@ describe('deleting a stylist', {:type => :feature}) do
   it('allows the user to delete a stylist') do
     stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
     stylist.save()
-    visit('/')
-    click_link('Milhouse Van Houten')
+    visit("/stylists/#{stylist.id()}")
     click_button('Get Outta My Sight!')
     expect(page).not_to have_content('Milhouse Van Houten')
   end
@@ -44,8 +42,7 @@ describe('adding and viewing a client profile', {:type => :feature}) do
   it('allows user to add a client to a stylist page') do
     stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
     stylist.save()
-    visit('/')
-    click_link('Milhouse Van Houten')
+    visit("/stylists/#{stylist.id()}")
     fill_in('client_name', :with => 'Karl')
     click_button('Add Client')
     click_link('Karl')
@@ -57,25 +54,9 @@ describe('deleting a client', {:type => :feature}) do
   it('allows user to delete a client') do
     stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
     stylist.save()
-    visit('/')
-    click_link('Milhouse Van Houten')
-    fill_in('client_name', :with => 'Karl')
-    click_button('Add Client')
-    click_link('Karl')
-    click_button('Delete \'Em!')
-    expect(page).not_to have_content('Karl')
-  end
-end
-
-describe('deleting a client', {:type => :feature}) do
-  it('allows user to delete a client') do
-    stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
-    stylist.save()
-    visit('/')
-    click_link('Milhouse Van Houten')
-    fill_in('client_name', :with => 'Karl')
-    click_button('Add Client')
-    click_link('Karl')
+    client = Client.new({:name => 'Karl', :stylist_id => "#{stylist.id()}", :id => nil})
+    client.save()
+    visit("/clients/#{client.id()}")
     click_button('Delete \'Em!')
     expect(page).not_to have_content('Karl')
   end
@@ -85,11 +66,9 @@ describe('modifying a client name', {:type => :feature}) do
   it('allows user to change a client\'s name') do
     stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
     stylist.save()
-    visit('/')
-    click_link('Milhouse Van Houten')
-    fill_in('client_name', :with => 'Karl')
-    click_button('Add Client')
-    click_link('Karl')
+    client = Client.new({:name => 'Karl', :stylist_id => "#{stylist.id()}", :id => nil})
+    client.save()
+    visit("/clients/#{client.id()}")
     fill_in('new_name', :with => 'Chief Wiggum')
     click_button('Change Name')
     expect(page).to have_content('Chief Wiggum')
