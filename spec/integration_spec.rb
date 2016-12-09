2c9b5c020cef672b46daf0d4d17ab515226f9c1d
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-09 09:27:05 -0800

    Pass integration spec for adding client and viewing their page

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -44,3 +44,16 @@ describe('deleting a stylist', {:type => :feature}) do
     expect(page).not_to have_content('Milhouse Van Houten')
   end
 end
+
+describe('adding and viewing a client profile', {:type => :feature}) do
+  it('allows user to add a client to a stylist page') do
+    stylist = Stylist.new({:name => 'Milhouse Van Houten', :id => nil})
+    stylist.save()
+    visit('/')
+    click_link('Milhouse Van Houten')
+    fill_in('client_name', :with => 'Karl')
+    click_button('Add Client')
+    click_link('Karl')
+    expect(page).to have_content('Karl')
+  end
+end
