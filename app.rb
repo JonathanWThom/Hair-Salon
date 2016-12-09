f8833de270d7f63d6a4fde8dd09a0258d39d2a0b
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-08 19:57:16 -0800

    Add index page and stylists page route

---
+++ app.rb
@@ -0,0 +1,50 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/client')
+require('./lib/stylist')
+require('pg')
+
+DB = PG.connect(:dbname => 'hair_salon')
+
+get('/') do
+  erb(:index)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/clients') do
+  @clients = Client.all()
+  erb(:clients)
+end
+
+post('/stylists') do
+  name = params.fetch('stylist_name')
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/stylists/:id') do
+  @stylist = Stylist.find(params.fetch('id').to_i)
+  erb(:stylist)
+end
+
+#add stylist
+#add client to stylist
+#patch stylists
+#delete stylists
+
+#add client
+#patch clients
+#delete clients
+
+
+
+# As a salon owner, I want to view, add, update and delete stylists.
+# As a salon owner, I want to view, add, update and delete clients.
+# As a salon owner, I want to add clients to a stylist.
